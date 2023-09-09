f2a26c434fa83b3c64d80a0231deefe3f69a9cab
Wralith <wralithdev@gmail.com>
2023-09-09 16:07:26 +0300

    initial solution

--- src/main/java/com/trendyol/shipment/Basket.java
+++ src/main/java/com/trendyol/shipment/Basket.java
@@ -1,5 +1,6 @@
 package com.trendyol.shipment;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class Basket {
@@ -7,7 +8,36 @@ public class Basket {
     private List<Product> products;
 
     public ShipmentSize getShipmentSize() {
-        return null;
+        int sizeThreshold = 3;
+        if  (products.size() <= 2) {
+            return largestShipmentSize();
+        }
+        var size = ShipmentSize.SMALL;
+        var m = new HashMap<ShipmentSize, Integer>();
+        for (var product : products) {
+            var currentSize = product.getSize();
+            m.putIfAbsent(currentSize, 0);
+            m.put(currentSize, m.get(currentSize) + 1);
+        }
+        for (var entry : m.entrySet()) {
+            if (entry.getValue() >= sizeThreshold) {
+                return entry.getKey().getOneSizeLarger();
+            }
+            if (entry.getKey().getNumberRepresentation() > size.getNumberRepresentation()) {
+                size = entry.getKey();
+            }
+        }
+        return size;
+    }
+
+    private ShipmentSize largestShipmentSize() {
+        var size = ShipmentSize.SMALL;
+        for (var product : products) {
+            if (product.getSize().getNumberRepresentation() > size.getNumberRepresentation()) {
+                size = product.getSize();
+            }
+        }
+        return size;
     }
 
     public List<Product> getProducts() {
--- src/main/java/com/trendyol/shipment/ShipmentSize.java
+++ src/main/java/com/trendyol/shipment/ShipmentSize.java
@@ -5,5 +5,16 @@ public enum ShipmentSize {
     SMALL,
     MEDIUM,
     LARGE,
-    X_LARGE
+    X_LARGE;
+
+    public int getNumberRepresentation() {
+        return this.ordinal();
+    }
+
+    public ShipmentSize getOneSizeLarger() {
+        if (this == X_LARGE) {
+            return X_LARGE;
+        }
+        return values()[this.ordinal() + 1];
+    }
 }
